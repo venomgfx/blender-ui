27fad41a34ae3f88b04c06bdf1ed813e3e772453
Pablo Vazquez <venomgfx@gmail.com>
2017-03-09 20:54:33 +0100

    Panels!

--- src/scripts/tutti/01_widgets.js
+++ src/scripts/tutti/01_widgets.js
@@ -12,3 +12,7 @@ $('.tabs-bar__tab').on('click', function(){
 	$('.tabs-bar__tab').removeClass('enabled');
 	$(this).addClass('enabled');
 });
+
+$('.js-panel-collapse').on('click', function(){
+	$(this).parent().parent().toggleClass('enabled');
+});
