88fa02736fb45e18711879ed9173a92cb2e84dc8
Pablo Vazquez <venomgfx@gmail.com>
2017-10-05 20:00:40 +0200

    Introducing Template snippets
    Used for now for lists ui, to add viewlayers/collections/overrides.

--- src/scripts/tutti/01_widgets.js
+++ src/scripts/tutti/01_widgets.js
@@ -1,26 +1,66 @@
 // e.g. Checkboxes
-$('.js-bool').on('click', function(){
+$(document).on('click', 'body .js-bool', function(){
 	$(this).toggleClass('enabled');
 });
 
 
 // Tabs: there can only be 1 tab enabled, so disable the rest
-$('.tabs-bar__tab').on('click', function(){
+$(document).on('click', 'body .tabs-bar__tab', function(){
 	$('.tabs-bar__tab').removeClass('enabled');
 	$(this).addClass('enabled');
 });
 
 
 // Tabs: there can only be 1 tab enabled, so disable the rest
-$('.wgt-list-item').on('click', function(){
+$(document).on('click', 'body .wgt-list-item', function(e){
+	e.stopPropagation();
+
 	$('.wgt-list-item').removeClass('enabled');
 	$(this).addClass('enabled');
+
+	if ($(this).hasClass('viewlayer')){
+		$(".js-add-template[data-template='list-viewlayer']")
+			.addClass('disabled');
+
+		$(".js-add-template[data-template='list-collection'], \
+			 .js-add-template[data-template='list-override']")
+			.removeClass('disabled');
+	};
+
+	if ($(this).hasClass('collection')){
+		$(".js-add-template[data-template='list-viewlayer']")
+			.addClass('disabled');
+
+		$(".js-add-template[data-template='list-collection'], \
+			 .js-add-template[data-template='list-override']")
+			.removeClass('disabled');
+	};
+
+	if ($(this).hasClass('override')){
+		$(".js-add-template[data-template='list-viewlayer'], \
+			 .js-add-template[data-template='list-collection'], \
+			 .js-add-template[data-template='list-override']")
+			.addClass('disabled');
+	};
+
 });
 
 
+$(document).on('click', 'body .wgt-list', function(){
+
+	$('.wgt-list-item').removeClass('enabled');
+
+	$(".js-add-template[data-template='list-viewlayer']")
+		.removeClass('disabled');
+
+	$(".js-add-template[data-template='list-collection'], \
+		 .js-add-template[data-template='list-override']")
+		.addClass('disabled');
+});
+
 // Panels collapse
-$('.js-panel-collapse').on('click', function(){
-	$(this).parent().parent().toggleClass('enabled');
+$(document).on('click', 'body .js-panel-collapse', function(){
+	$(this).parent().toggleClass('enabled');
 });
 
 
@@ -43,7 +83,7 @@ function toggleImages(image, from, to){
 
 
 // UI List item collapse
-$('.js-list-item-collapse').on('click', function(){
+$(document).on('click', 'body .js-list-item-collapse', function(){
 	$(this).parent().siblings('ul').toggleClass('expanded');
 
 	toggleImages($(this), '_right.png', '_down.png');
@@ -51,7 +91,7 @@ $('.js-list-item-collapse').on('click', function(){
 
 
 // Swap icons on/off
-$('.js-icon-toggle-onoff').on('click', function(){
+$(document).on('click', 'body .js-icon-toggle-onoff', function(){
 
 	toggleImages($(this), '_off.png', '_on.png');
 });
